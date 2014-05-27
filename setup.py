6083e946a207ba5ec7ec9d61908a132d72b7bb34
Marko Toplak <marko.toplak@gmail.com>
2014-05-27 16:32:24 +0200

    Drop requirements.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup, find_packages
 
 NAME = "GenAPI"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 DESCRIPTION = "A Python API for the Genesis platform."
 LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
